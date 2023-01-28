394deee0671480d82cfb6737f2f59e5dcbe3aa14
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-01-28 20:19:40 +0500

    if else

--- uyga vazifalar/homework 2/js/main.js
+++ uyga vazifalar/homework 2/js/main.js
@@ -1,10 +1,25 @@
 
-// If else bu yerda over chiqadi sababi 1 + "a"ni qoshib b'olmaydi shunga over chiqadi
+// // If else bu yerda over chiqadi sababi 1 + "a"ni qoshib b'olmaydi shunga over chiqadi
 
+
+// // let result;
+// // let a=1;
+// // let b="a";
+// // if (a + b < 4) {
+// //     result = "below";
+// // } else {
+// //     result = "over";
+// // }
+
+// // console.log(result);
+
+
+
+// // If Else   bu yerda 3 katta 4dan shunga javob below chiqadi
 
 // let result;
 // let a=1;
-// let b="a";
+// let b=2;
 // if (a + b < 4) {
 //     result = "below";
 // } else {
@@ -13,17 +28,3 @@
 
 // console.log(result);
 
-
-
-// If Else   bu yerda 3 katta 4dan shunga javob below chiqadi
-
-let result;
-let a=1;
-let b=2;
-if (a + b < 4) {
-    result = "below";
-} else {
-    result = "over";
-}
-
-console.log(result);
