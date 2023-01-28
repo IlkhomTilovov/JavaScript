07c2e2dc603cb4f7d126759fe52951e9ca86f0a3
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-01-28 15:16:45 +0000

    if else

---
+++ homework 2/js/main.js
@@ -0,0 +1,30 @@
+
+// // If else bu yerda over chiqadi sababi 1 + "a"ni qoshib b'olmaydi shunga over chiqadi
+
+
+// // let result;
+// // let a=1;
+// // let b="a";
+// // if (a + b < 4) {
+// //     result = "below";
+// // } else {
+// //     result = "over";
+// // }
+
+// // console.log(result);
+
+
+
+// // If Else   bu yerda 3 katta 4dan shunga javob below chiqadi
+
+// let result;
+// let a=1;
+// let b=2;
+// if (a + b < 4) {
+//     result = "below";
+// } else {
+//     result = "over";
+// }
+
+// console.log(result);
+
