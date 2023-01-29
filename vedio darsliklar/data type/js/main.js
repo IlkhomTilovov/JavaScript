eab597a0db96431535a688e3411045a575c6e2b7
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-01-29 23:45:48 +0500

    video darsliklar

---
+++ vedio darsliklar/data type/js/main.js
@@ -0,0 +1,19 @@
+// Number 1 , 2 ,3 ....
+// String '', "", ``,
+// let a =`'Salom Dunyo "Hayrli tong"'
+// sadfas asd
+// a sd as
+// d asd
+//  as
+//  d a
+//  sd
+//   as
+//   d asd asd
+//    a`
+// boolean true va false
+// console.log(true == false);
+// BigInt
+// console.log(2 ** 53 + 1);
+// null
+// undefined
+// console.log(a);
