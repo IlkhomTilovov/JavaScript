3d6bf38913a243bae4309c0b27c6209b3c370733
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-01-29 22:21:26 +0500

    Type Convirsions | Operators | Comparision | Logical Operators

---
+++ uyga vazifalar/homework3/js/main.js
@@ -0,0 +1,45 @@
+// let a = 1;
+// let b = "1";
+// console.log(a + b);
+// let a = 1;
+// let b = "1.2";
+// let c = "1.2";
+// // console.log(a + +b);
+// console.log(a + parseInt(c));
+// console.log(a + parseFloat(b));
+
+// console.log(Boolean(" "));
+// console.log(Boolean( ));
+
+// let a = 3;
+// let b = 2;
+
+// console.log(a % b);
+
+
+// let a = 3;
+// let b = 2;
+
+// console.log(b % a);
+
+// console.log(2 * 2);
+// console.log(2 * 2 * 2);
+// console.log(2 * 2 * 2 * 2);
+// console.log(2 ** 20);
+
+// let a = 3;
+// let b = 2;
+// console.log((a = b));
+// console.log(a == b);
+// console.log(a === b);
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// c += a
+// console.log(c);
+// let a = 1;
+// a = a--;
+// console.log(a);
+// console.log(a);
+// console.log(typeof("\t \n" - 2));
--- vedio darsliklar/js/main.js
+++ vedio darsliklar/js/main.js
@@ -1,1 +1,5 @@
-console.log("test uchun");
+let a = 2;
+console.log(a);
+var l = 1;
+var l=2
+console.log(l);
