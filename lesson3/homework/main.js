5b0e7018b6c7b820c4ff2b4d7326931edce63df3
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-01-30 21:38:07 +0500

    If else | Nullish

---
+++ lesson3/homework/main.js
@@ -0,0 +1,53 @@
+// let weather = 2;
+// if (0 > weather) {
+//     console.log("Havo sovuq");
+// } else {
+//     console.log("Havo issiq");
+// }
+
+// let weather = 20;
+// if (0 > weather) {
+//     console.log("Havo sovuq");
+// } else if (10 > weather) {
+//     console.log("Havo iliq");
+// } else if (15 > weather) {
+//     console.log("Havo issiq");
+// } else if (20 > weather) {
+//     console.log("Havo juda issiq");
+// } else{
+//     console.log("Bunday harorat yo'q");
+// }
+
+// ?? Bu Null va Undefined chiqarib beradi yani qiymat false bo'lsa
+
+// ? bu esa if teng 
+// : bu esa else qaytaradi 
+
+// let weather = 12;
+// weather > 0 && weather < 10
+//     ? console.log("havo sovuq")
+//     : weather > 10 && weather < 15
+//         ? console.log("havo iliq")
+//         : weather > 15 && weather < 20
+//             ? console.log("havo issiq")
+//             : weather > 20 && weather < 25
+
+
+// let a = 1;
+// a= ++a;
+// // a++ =
+// // a=a;
+// // console.log(a);
+// // console.log(a);
+// console.log("text"+1+0);
+
+
+// let login = "webbrain"
+// let pw = undefined;
+// if (login ?? false) {
+//     console.log("Welcome to WebBreain");
+// } else{
+//     console.log("Login yoki Parol notog'ri");
+// }
+
+console.log("" ?? false);
--- vedio darsliklar/js/main.js
+++ vedio darsliklar/js/main.js
@@ -4,6 +4,30 @@
 // l = 2
 // console.log(l);
 
+// let a = 2;
+// let b = 3;
+// console.log(a %= b)
+
+// let a = 10 ;
+// let b = 10 ;
+// console.log(a === b);
+
+// console.log("\t");
+
+// ++a;
+
+// let arr = [1, 2];
+// for (let i = 0; i < arr.length; i++) {
+
+// }
+
+// let res = arr.forEach()
+
+
+// let a = "";
+// let b = "2";
+// console.log(a == b);
 let a = 2;
-let b = 1;
-console.log(++a );
+let b = 3;
+console.log(a = a ** 2);
+console.log(a);
