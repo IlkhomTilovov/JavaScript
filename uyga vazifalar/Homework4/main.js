c225675a41b82283fa327dac1d3f9ea6ea973a98
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-01-31 19:06:14 +0500

    Home work 4

---
+++ uyga vazifalar/Homework4/main.js
@@ -0,0 +1,31 @@
+// let zero = 0;
+
+// if (zero > 0) {
+//     console.log("no'ldan kotta");
+// } else if (zero < 0) {
+//     console.log("no'ldan kichik");
+// } else {
+//     console.log("no'lga teng");
+// }
+
+// zero > 0
+//     ? console.log("nolden katta") : console.log("malumot yo'q");
+// zero < 0
+//     ? console.log("no'ldan kichik") : console.log("malumot yo'q");
+// zero = 0
+//     ? console.log("no'lga teng") : console.log("malumot yo'q");
+// let zero = 0;
+
+// let zero ="asd";
+
+
+// zero > 0
+//     ? console.log("No'ldan katta") :
+//     zero < 0 ? console.log("Noldan kichik") :
+//         zero == 0 ? console.log("Nolga teng") :
+//             console.log("Number kiritilmadi");
+
+
+// if(""){
+//     console.log("Hello js");
+// }
