6ed5df7e9d676aee7470dccb7e6404fa5c6d8af6
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-02-04 18:38:50 +0500

    Karraliklar

--- uyga vazifalar/homework6/main.js
+++ uyga vazifalar/homework6/main.js
@@ -1,6 +1,11 @@
-for (i = 1; i <= 10; i++) {
-    console.log(`\n${i} - karraliklar\n`);
-    for (let j = 0; j <= 10; j++) {
-        console.log(`${i} * ${j} = ${i * j} `);
-    }
+// for (i = 1; i <= 10; i++) {
+//     console.log(`\n${i} - karraliklar\n`);
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i * j} `);
+//     }
+// }
+
+let a = 1;
+for (let kara = 1; a <= 10; a++) {
+    console.log(`${kara} * ${a} = ${a * kara} `);
 }
