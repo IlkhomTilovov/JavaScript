f75797da95d0f31b2fd1e37708be0ff22b5cacf8
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-02-05 22:19:18 +0500

    functions

--- uyga vazifalar/homework6/main.js
+++ uyga vazifalar/homework6/main.js
@@ -5,7 +5,48 @@
 //     }
 // }
 
-let a = 1;
-for (let kara = 1; a <= 10; a++) {
-    console.log(`${kara} * ${a} = ${a * kara} `);
+// let a = 1;
+// for (let kara = 1; a <= 10; a++) {
+//     console.log(`${kara} * ${a} = ${a * kara} `);
+// }
+
+
+// let title;
+// title = "Webbrain";
+// console.log(title);
+// let total = 208
+// function get(grade) {
+//     let res = grade * 100 / 208
+//     console.log(res);
+// }
+
+// get(52);
+// get(104);
+
+
+// let title;
+// title = "Webbrain";
+// console.log(title);
+// let total = 208
+// function get(grade) {
+//     let res = grade * 100 / 208
+//     console.log(res);
+// }
+// get()
+// get(52);
+// get(104);
+
+
+// function getName(name){
+//     return name;
+// }
+
+// console.log(getName("Toshmatov"));
+
+
+function get(grade, total) {
+    let res = (grade * 100) / 208;
+    return res;
 }
+
+console.log(get(200) > 50 ? "passed" : "faild");
