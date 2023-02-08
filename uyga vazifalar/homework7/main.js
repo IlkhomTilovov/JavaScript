d4d9aedd23616ec8f688cb1b41b7e0db56234847
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-02-08 18:21:46 +0500

    Home works

--- uyga vazifalar/homework7/main.js
+++ uyga vazifalar/homework7/main.js
@@ -26,6 +26,109 @@
 // // console.log(typeof (salaries));
 
 // for (key in salaries) {
-//     let qiymat = salaries[key] * 2;
+//     let qiymat = salaries.Pete ;
 //     console.log(qiymat);
 // }
+
+
+// switch case ==============================
+// let a = 4;
+
+// switch (a) {
+//     case 1:
+//         console.log("Dushanba");
+//         break;
+//     case 2:
+//         console.log("Seshanba");
+//         break;
+//     case 3:
+//         console.log("Chorshanba");
+//         break;
+//     case 4:
+//         console.log("Payshanba");
+//         break;
+//     case 5:
+//         console.log("Juma");
+//         break;
+//     case 6:
+//         console.log("Shanba");
+//         break;
+//     case 7:
+//         console.log("Yakshanba");
+//         break;
+//         default:
+//             console.log("7 kundan boshqa kunlar mavjud eams");
+// }
+
+// =============================2=======================================
+
+// let k=5;
+
+// switch(k){
+//     case 1:
+//         console.log("Juda yomon");
+//         break
+//     case 2:
+//         console.log("Qoniqarsiz");
+//         break
+//     case 3:
+//         console.log("Qoniqarli");
+//         break
+//     case 4:
+//         console.log("Yaxshi");
+//         break
+//     case 5:
+//         console.log("A'lo");
+//         break
+//         default:
+//             console.log("Berilgan baxo xato");
+// }
+
+
+// ========================3========================
+
+// let oy = 22;
+// switch (oy) {
+//     case 1:
+//         console.log("Qish");
+//         break;
+//     case 2:
+//         console.log("Qish");
+//         break;
+//     case 3:
+//         console.log("Bahor");
+//         break;
+//     case 4:
+//         console.log("Bahor");
+//         break;
+//     case 5:
+//         console.log("Bahor");
+//         break;
+//     case 6:
+//         console.log("Yoz");
+//         break;
+//     case 7:
+//         console.log("Yoz");
+//         break;
+//     case 8:
+//         console.log("Yoz");
+//         break;
+//     case 9:
+//         console.log("Kuz");
+//         break;
+//     case 10:
+//         console.log("Kuz");
+//         break;
+//     case 11:
+//         console.log("Kuz");
+//         break;
+//     case 12:
+//         console.log("Qish");
+//         break;
+//     default:
+//     console.log("1-yilda 4 ta fasldan iborat va 12 ta oydan tashkil topgan");
+// }
+
+
+
+// ========================================4====================================
