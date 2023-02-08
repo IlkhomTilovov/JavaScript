1dfa57d6b8475673914ae1ee54b95ed463050ec3
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-02-08 17:52:08 +0500

    Objects Basics

--- uyga vazifalar/homework7/main.js
+++ uyga vazifalar/homework7/main.js
@@ -1,9 +1,31 @@
-let user = {
-    name: "John",
-    surname: "Smith",
-    name: "Pete"
-}
-// console.log(user);
-// console.log(user.name);
-delete user.name;
-console.log(user.surname);
+// let user = {
+//     name: "John",
+//     surname: "Smith",
+//     name: "Pete"
+// }
+// // console.log(user);
+// // console.log(user.name);
+// delete user.name;
+// console.log(user.surname);
+
+
+
+// let jeck = {
+//     john: 100,
+//     smith: 160,
+//     pete: 130,
+// }
+
+// // let res = jeck;
+
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: "hello"
+// };
+// // console.log(typeof (salaries));
+
+// for (key in salaries) {
+//     let qiymat = salaries[key] * 2;
+//     console.log(qiymat);
+// }
