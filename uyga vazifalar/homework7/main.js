394e358db45a1973c4707efd3d70bb5c92c9464f
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-02-08 18:28:27 +0500

    Switchs

--- uyga vazifalar/homework7/main.js
+++ uyga vazifalar/homework7/main.js
@@ -132,3 +132,46 @@
 
 
 // ========================================4====================================
+
+
+// let oy = 2;
+
+// switch (oy) {
+//     case 1:
+//         console.log("31 kundan iborat");
+//         break;
+//     case 2:
+//         console.log("28 kundan iborat");
+//         break;
+//     case 3:
+//         console.log("31 kundan iborat");
+//         break;
+//     case 4:
+//         console.log("30 kundan iborat");
+//         break;
+//     case 5:
+//         console.log("31 kundan iborat");
+//         break;
+//     case 6:
+//         console.log("30 kundan iborat");
+//         break;
+//     case 7:
+//         console.log("31 kundan iborat");
+//         break;
+//     case 8:
+//         console.log("31 kundan iborat");
+//         break;
+//     case 9:
+//         console.log("30 kundan iborat");
+//         break;
+//     case 10:
+//         console.log("31 kundan iborat");
+//         break;
+//     case 11:
+//         console.log("30 kundan iborat");
+//         break;
+//     case 12:
+//         console.log("31 kundan iborat");
+//         break;
+    
+// }
