1e9b677518ec7c6791598d236149442c2fddb316
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-02-19 01:09:35 +0500

    Input|function|obj

---
+++ Number/main.js
@@ -0,0 +1,91 @@
+// let a = 12;
+// if (a > 10) {
+//     console.log(a + 3);
+// } else if (a < 10) {
+//     console.log(a * 2);
+// } else {
+//     console.log(a = 22);
+// }
+
+
+// =======Primitiv==========
+
+// number  faqat sonlar
+// string '' , "", ``
+// boolean true , false
+// null 
+// undefined
+// bigint 2**53
+// symbol
+
+
+// =========none primitive
+// objeckt
+
+
+
+// let a = 2 ** 53;
+// console.log(-9007199254740992 + 1);
+
+// Numeber =================================
+
+
+// let a = 1;
+// let b = 4;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(4 ** 3);
+
+
+// string  "" , '', ``
+
+// let a = 2;
+// let b = "3";
+// let c = 0
+// // console.log(a + +b);
+// // console.log(number(b) + a);
+// c = (Number(b) + a)
+// console.log(c);
+
+
+
+// Boolean
+// let ruchka = true; // ha     to'g'ri
+// let qalam = false; // yo'q   noto'g'ri
+// console.log(ruchka = qalam);
+
+//  = tenglashtiradi
+//  ==  bir biriga tengmi ?
+//  === qatiyan teng!
+// >>>>>>> kota
+// <<<<<<< kichkina
+
+
+// ++a;   birinchi bolib 1ni qqoshib ber digani
+// a++;   bu ishora esa keyingi console.log dan keyin 1 ni qoshib beradi.
+
+
+let a = 7;
+let b = 5;
+// console.log(a += b); // 12 teng
+// a++; // 13 teng
+// console.log(13 += 5); // 18 teng
+// a = a + 1;
+// console.log(a + ++b); // 13 7+1+5
+// a += 2;
+// console.log(++a + b); // 12 1+7+1+5
+
+
+
+
+
+
+
+// let a = 4
+
+
+console.log();
