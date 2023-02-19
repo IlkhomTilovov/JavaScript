3f280a267a8734a16ac2d44625cd9b9d18702fdb
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-02-19 22:04:43 +0500

    homework 7

---
+++ lesson 7/main.js
@@ -0,0 +1,53 @@
+// let obj = {
+//     name: "webbrain",
+//     age: 23,
+//     child: {
+//         name: "eshmat",
+//         age: 11,
+//         child: {
+//             name: "toshmat",
+//             age: 33,
+//         },
+//     },
+// }
+// let sum = 0;
+// while (1) {
+//     if (obj) {
+//         // console.log(obj.name);
+//         sum = sum + obj.age
+//     } else {
+//         break;
+//     }
+//     obj = obj.child;
+// }
+// console.log(sum);
+
+// function sayHi() {
+//     console.log("Hi");
+// }
+// let name = "Webbrain";
+
+// let user = {
+//     // name:"webbrain",
+//     name,
+//     // sayHi:function(){
+//     //     console.log("hi");
+//     // }
+//     sayHi() {
+//         console.log("I said Hi");
+//     }
+// }
+// user.sayHi()
+
+// function getName() {
+//     this.name = name
+//     this.sayHi = function () {
+//         console.log("hi", this.name);
+//         return "hey"
+//     }
+//     console.log(this);
+// }
+// new getName("Eshmat").sayHi()
+// console.log(getName("Eshmat").sayHi());
+
+let func={}
