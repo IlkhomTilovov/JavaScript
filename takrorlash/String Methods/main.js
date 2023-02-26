e06fef6ef23046bb93bb02b7880c08bab58006d6
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-02-26 18:33:44 +0500

    String | All you need to know about String

---
+++ takrorlash/String Methods/main.js
@@ -0,0 +1,67 @@
+// lastIndexOf
+// Bu oxirgi kelayotgan indexni olib ber beradi
+
+// let name = "Web Brain Academy"
+// console.log(name.lastIndexOf("W"));
+
+// slice()
+// Bu nechinchin indexdan kesib berishga ishlaydi
+
+
+// let id = "WebBrain Academy"
+// console.log(id.slice(1,5));
+
+// substr()
+
+// Bu nechinchi indexdan nechichinchi indexgacha kesib berishga ishlaydi
+
+// let str = "WebBrain Academy";
+// console.log(str.substr(2,4));
+
+
+// replace 
+
+// Bu malumotarni almashtirib beradi
+
+// let str = "WebBrain Academy";
+// console.log(str.replace("e", "B"));
+
+// charAt()
+
+// Bu doim boshidagi harfni qolib beradi
+
+// let str = "WebBrain Academy";
+// console.log(str.charAt());
+
+// toUpperCase()
+
+// Malumotlarni kota harfda qilib beradi.
+
+// let str = "WebBrain Academy";
+// console.log(str.toUpperCase());
+
+// toLowerCase()
+
+// Malumotlarni kichik qilib berish uchun ishlatiladi.
+
+// let str = "WebBrain Academy";
+// console.log(str.toLowerCase())
+
+
+// str.trim()
+
+// Boshida va oxirida qancha bosh joy bo'lsa shularni olib tashlab beradi.
+
+// let str = "  WebBrain Academy     ";
+// console.log(str.trim().length)
+// console.log(str.length)
+// // console.log(str)
+
+
+// split()
+
+// Bu malumotlarni array qilib qaytaradi va qandaydir harfni yozsangiz shu ozini olib tashlab qolganini array qilib beradi.
+
+// let str = "WebBrain Academy";
+// // console.log(str.split(""))
+// console.log(str.split("a"))
