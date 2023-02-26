6f145fbdb08a1e25613b578576dcc245b7a42e96
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-02-26 16:34:18 +0500

    Numbers | All you need to know

---
+++ takrorlash/number/main.js
@@ -0,0 +1,46 @@
+// Math.round
+
+// Bu soning eng yaqin butun qismiga yaxlitlaydi
+
+// let num1 = -7.4
+// let num2 = -6.4
+// console.log(Math.round(num1));
+// console.log(Math.round(num2));
+
+
+
+// Math.floor
+
+// Bu eng kichik butun qismiga qarab yaxlitlaydi
+
+// let num1 = 12.999
+// let num2 = -12.999
+// console.log(Math.floor(num1));
+// console.log(Math.floor(num2));
+
+// Math.ceil
+
+// Bu eng kattasiga qarab butun yaxlitlaydi
+
+// let num1 = 12.999
+// let num2 = -12.999
+// console.log(Math.ceil(num1));
+// console.log(Math.ceil(num2));
+
+
+// Math.trunc
+
+// Bu faqatgina soni butun qismini qaytaradi
+
+// let num1 = 12.999
+// let num2 = -12.999
+// console.log(Math.trunc(num1));
+// console.log(Math.trunc(num2));
+
+
+// Math.random 
+
+// Bu ixtiyoriy raqamlarni qaytaradi
+
+// console.log(Math.random());
+// console.log(Math.trunc(Math.random()*100));
