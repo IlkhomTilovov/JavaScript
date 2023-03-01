b59517728eab0b5a4d4cd91fd64f33fbb357dc0d
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-03-02 02:45:10 +0500

    Switch | For loop

---
+++ Switch  For loop/main.js
@@ -0,0 +1,49 @@
+// let data = "bulut"
+// switch (data) {
+//     case "yomgir":
+//         console.log("Bugun yomgir");
+//         break;
+//     case "qor":
+//         console.log("Bugun qor");
+//         break;
+//     case "bulut":
+//         console.log("Bugun bulut");
+//         break;
+//     default:
+//         console.log("No date");
+// }
+
+
+// for (i = 1; i <= 10; i++) {
+//     console.log("Hi Webbrain");
+// }
+
+// break =================================== 
+
+// Toq sonni chiqarish
+
+// for (i = 1; i <= 10; i++) {
+//     if (i % 2 === 0) continue;
+//     console.log(i);
+
+// }
+outer: for (i = 10; i >= 1; i -= 1) {
+    inner: for (j = 1; j <= 10; j += 10) {
+        if (i <= 0) break outer
+    }
+    console.log(`${i} + ${j} = ${i + j}`);
+}
+// outer: for (i = 10; i >= 1; i += 10) {
+//     inner: for (j = 1; j <= 10; j += 1) {
+//         if (i === 4) break outer;
+//         console.log(`${i} + ${j} = ${i + j}`);
+//     }
+//     console.log("");
+// }
+
+// for (i = 2; i <= 10; i++) {
+//     console.log(`\n${i} - karraliklar \n`);
+//     for (j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i * j}`);
+//     }
+// }
