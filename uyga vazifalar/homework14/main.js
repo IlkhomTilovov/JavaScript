a9f0db2476de1a306df79f5864ebfe9e67a9a1fd
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-03-10 21:11:05 +0500

    Home Work 14

--- uyga vazifalar/homework14/main.js
+++ uyga vazifalar/homework14/main.js
@@ -6,22 +6,207 @@
 //     }
 // }
 // moment().format("LT")
+// let date = new Date()
+// let a = date.getMonth()
+
+// if (a < 10) {
+//     a = "0" + (a + 1);
+// }
+// console.log(a);
+
+// setInterval(() => {
+//     let date = new Date();
+//     LT.innerHTML = `${date.getHours()} : ${date.getMinutes()}`
+// }, 1000)
+// setInterval(() => {
+//     let date = new Date();
+//     LTS.innerHTML = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`
+// }, 1000)
+// setInterval(() => {
+//     let date = new Date();
+//     L.innerHTML = `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`
+//     if (date.getMonth() < 10) {
+//         date.getMonth() = "0" + date.getMonth()
+//     }
+
+// }, 1000)
+
+
+
+let date = new Date();
+let months = date.getMonth();
+let day = date.getDate();
+
+if (months < 10) {
+    months += 1;
+    months = "0" + (months);
+}
+if (day < 10) {
+    day = "0" + (day + 1);
+}
+
+
+let oy = date.getMonth();
+
+switch (oy) {
+    case 0:
+        oy = "January";
+        break;
+    case 1:
+        oy = "February";
+        break;
+    case 2:
+        oy = "March";
+        break;
+    case 3:
+        oy = "April"
+        break;
+    case 4:
+        oy = "May"
+        break;
+    case 5:
+        oy = "Juny"
+        break;
+    case 6:
+        oy = "July"
+        break;
+    case 7:
+        oy = "August"
+        break;
+    case 8:
+        oy = "Semtember"
+        break;
+    case 9:
+        oy = "October"
+        break;
+    case 10:
+        oy = "November"
+        break;
+    case 11:
+        oy = "December"
+        break;
+}
+
+
+let oylar = date.getMonth();
+
+switch (oylar) {
+    case 0:
+        oylar = "Jan";
+        break;
+    case 1:
+        oylar = "Feb";
+        break;
+    case 2:
+        oylar = "Mar";
+        break;
+    case 3:
+        oylar = "Apr"
+        break;
+    case 4:
+        oylar = "May"
+        break;
+    case 5:
+        oylar = "Jun"
+        break;
+    case 6:
+        oylar = "Jul"
+        break;
+    case 7:
+        oylar = "Aug"
+        break;
+    case 8:
+        oylar = "Sem"
+        break;
+    case 9:
+        oylar = "Oct"
+        break;
+    case 10:
+        oylar = "Nov"
+        break;
+    case 11:
+        oylar = "Dec"
+        break;
+}
+
+let hafta = date.getDay();
+
+switch (hafta) {
+    case 0:
+        hafta = "Sunday";
+        break;
+    case 1:
+        hafta = "Monday";
+        break;
+    case 2:
+        hafta = "Tuesday";
+        break;
+    case 3:
+        hafta = "Wednesday"
+        break;
+    case 4:
+        hafta = "Thusday"
+        break;
+    case 5:
+        hafta = "Friday"
+        break;
+    case 6:
+        hafta = "Saturday"
+        break;
+}
+
+let haftalar = date.getDay();
+
+switch (haftalar) {
+    case 0:
+        haftalar = "Sun";
+        break;
+    case 1:
+        haftalar = "Mon";
+        break;
+    case 2:
+        haftalar = "Tue";
+        break;
+    case 3:
+        haftalar = "Wed"
+        break;
+    case 4:
+        haftalar = "Thu"
+        break;
+    case 5:
+        haftalar = "Fri"
+        break;
+    case 6:
+        haftalar = "Sat"
+        break;
+}
+
 
 
 
 
 setInterval(() => {
     let date = new Date();
-    LT.innerHTML = `${date.getHours()} : ${date.getMinutes()}`
-}, 1000)
-setInterval(() => {
-    let date = new Date();
-    LTS.innerHTML = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`
-}, 1000)
-setInterval(() => {
-    let date = new Date();
-    L.innerHTML = `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`
-}, 1000)
+    LT.innerHTML = `${date.getHours()} : ${date.getMinutes()}`;
+
+    LTS.innerHTML = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`;
+
+    L.innerHTML = `${day}/${months}/${date.getFullYear()}`;
+
+    l.innerHTML = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+
+    LL.innerHTML = `${date.getDate()}/${oy}/${date.getFullYear()}`;
+
+    ll.innerHTML = `${date.getDate()}/${oylar}/${date.getFullYear()}`;
+
+    LLL.innerHTML = `${date.getDate()}/${oy}/${date.getFullYear()}, ${date.getHours()} : ${date.getMinutes()}`;
+
+    lll.innerHTML = `${date.getDate()}/${oylar}/${date.getFullYear()}, ${date.getHours()} : ${date.getMinutes()}`;
+
+    llll.innerHTML = `${haftalar} ${date.getDate()}/${oylar}/${date.getFullYear()}, ${date.getHours()} : ${date.getMinutes()}`;
+
+    LLLL.innerHTML = `${hafta} ${date.getDate()}/${oylar}/${date.getFullYear()}, ${date.getHours()} : ${date.getMinutes()}`;
+}, 1000);
 
 
 
@@ -68,16 +253,16 @@ setInterval(() => {
 
 
 
- 
+
 // let form = "LT";
 // let form = "LTS";
 // let form = "L";
 // let form = "l";
 // let form = "LL";
-// let form = "ll"; 
+// let form = "ll";
 // let form = "LLL";
-// let form = "lll"; 
-// let form = "LLLL"; 
+// let form = "lll";
+// let form = "LLLL";
 // let form = "llll";
 
 
--- uyga vazifalar/homework5/main.js
+++ uyga vazifalar/homework5/main.js
@@ -1,107 +1,106 @@
 
 // ==================================Yuzlik sonlar=========================================
 
-// let num = 571;
+let num = 534;
 
-// let y = parseInt(num / 100);
-// num = num % 100;
+let y = parseInt(num / 100);
+num = num % 100;
 
-// let o = parseInt(num / 10);
-// num = num % 10;
+let o = parseInt(num / 10);
+num = num % 10;
 
-// // let c = parseInt(num % 1000);
-// c = num;
+c = num;
 
-// a = '', b = '', d = ''
+a = '', b = '', d = ''
 
-// switch (y) {
-//     case 1:
-//         a = "bir uz";
-//         break;
-//     case 2:
-//         a = "ikki uz";
-//         break;
-//     case 3:
-//         a = "uch uz";
-//         break;
-//     case 4:
-//         a = "tor uz";
-//         break;
-//     case 5:
-//         a = "besh uz";
-//         break;
-//     case 6:
-//         a = "olti uz";
-//         break;
-//     case 7:
-//         a = "yetti uz";
-//         break;
-//     case 8:
-//         a = "sakkiz uz";
-//         break;
-//     case 9:
-//         a = "to'qiz yuz";
-//         break;
-// }
-// switch (o) {
-//     case 1:
-//         b = "o'n";
-//         break;
-//     case 2:
-//         b = "yigirma";
-//         break;
-//     case 3:
-//         b = "o'tiz";
-//         break;
-//     case 4:
-//         b = "qirq";
-//         break;
-//     case 5:
-//         b = "ellik";
-//         break;
-//     case 6:
-//         b = "oltmush";
-//         break;
-//     case 7:
-//         b = "yetmush";
-//         break;
-//     case 8:
-//         b = "sakson";
-//         break;
-//     case 9:
-//         b = "to'son";
-//         break;
-// }
-// switch (c) {
-//     case 1:
-//         d = "bir";
-//         break;
-//     case 2:
-//         d = "ikki";
-//         break;
-//     case 3:
-//         d = "uch";
-//         break;
-//     case 4:
-//         d = "tor";
-//         break;
-//     case 5:
-//         d = "besh";
-//         break;
-//     case 6:
-//         d = "olti";
-//         break;
-//     case 7:
-//         d = "yetti";
-//         break;
-//     case 8:
-//         d = "sakkiz";
-//         break;
-//     case 9:
-//         d = "to'qqiz"
-//         break;
-// }
-// console.log(a, b, d);
+switch (y) {
+    case 1:
+        a = "bir yuz";
+        break;
+    case 2:
+        a = "ikki yuz";
+        break;
+    case 3:
+        a = "uch yuz";
+        break;
+    case 4:
+        a = "tor yuz";
+        break;
+    case 5:
+        a = "besh yuz";
+        break;
+    case 6:
+        a = "olti yuz";
+        break;
+    case 7:
+        a = "yetti yuz";
+        break;
+    case 8:
+        a = "sakkiz yuz";
+        break;
+    case 9:
+        a = "to'qiz yuz";
+        break;
+}
+switch (o) {
+    case 1:
+        b = "o'n";
+        break;
+    case 2:
+        b = "yigirma";
+        break;
+    case 3:
+        b = "o'tiz";
+        break;
+    case 4:
+        b = "qirq";
+        break;
+    case 5:
+        b = "ellik";
+        break;
+    case 6:
+        b = "oltmush";
+        break;
+    case 7:
+        b = "yetmush";
+        break;
+    case 8:
+        b = "sakson";
+        break;
+    case 9:
+        b = "to'qson";
+        break;
+}
+switch (c) {
+    case 1:
+        d = "bir";
+        break;
+    case 2:
+        d = "ikki";
+        break;
+    case 3:
+        d = "uch";
+        break;
+    case 4:
+        d = "tort";
+        break;
+    case 5:
+        d = "besh";
+        break;
+    case 6:
+        d = "olti";
+        break;
+    case 7:
+        d = "yetti";
+        break;
+    case 8:
+        d = "sakkiz";
+        break;
+    case 9:
+        d = "to'qqiz"
+        break;
+}
+console.log(a, b, d);
 
 
 // ==================================Tub sonlar=========================================
