968c195e68360cebf773b996982ba0e2f3a09896
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-03-18 14:23:46 +0500

    new Map| new Set | new WeakMap | new WeakSet

--- g10/new Map new Set/main.js
+++ g10/new Map new Set/main.js
@@ -1,6 +1,14 @@
 // let obj = {};
 // let map = new Map();
 // obj.name = "Webbrain";
-// map.name = "webbrain";
-// console.log(obj, obj.name);
-// console.log(map.size , map.name);
+// map.title = "webbrain";
+// // console.log(obj, obj.name);
+// // console.log(map.size, map.name);
+
+// map.set("name", "IT Center");
+// console.log(map.size, map.get("title"));
+
+
+let obj = {};
+let map = new Map([["name", "webbrain"], ["title", "IT Center"]])
+console.log(map);
