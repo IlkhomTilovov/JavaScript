b2e11983b834797c0314dce4668e03e51d19b16c
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-03-22 16:29:13 +0500

    Recursion Functions

--- g10/Recursion Function/lesson-16.js
+++ g10/Recursion Function/lesson-16.js
@@ -1,11 +1,24 @@
 // Recursion functsions
 
-let n = 4;
+// let n = 4;
+// const sum = (n) => {
+//     let s = 0;
+//     for (let i = 1; i <= n; i++) {
+//         s += i;
+//     }
+//     console.log(s);
+// }
+// sum(4);
+
+
+let res = 0
 const sum = (n) => {
-    let s = 0;
-    for (let i = 1; i <= n; i++) {
-        s += i;
+    // console.log(n);
+    if (n === 1) return res += 1;
+    else {
+        res += n
+        return sum(n - 1)
     }
-    console.log(s);
 }
 sum(4);
+console.log(res);
