300f6f433c43a07a74f98e1b5664950cb3cbdc59
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-03-23 14:37:49 +0500

    Advenced function | Call | Apply | Bind

---
+++ g10/Advenced function & Call & Apply & Bind/main.js
@@ -0,0 +1,19 @@
+let user = {
+    name: "Webbrain",
+    lastname: "Academy",
+    title: "IT Center",
+    getFull() {
+        console.log(`${this.name} ${this.lastname}`);
+    }
+}
+
+user.getFull()
+
+let user2 = {
+    name: "Digigtal",
+    lastname: "One",
+    title: "IT Center",
+    getFull() {
+        console.log(`${this.name} ${this.lastname}`);
+    }
+}
