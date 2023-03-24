f6b68d962710d7f57bcb9fc778b088fa8d2d81c6
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-03-24 09:00:52 +0500

    Modul Homework

--- g10/modul/main.js
+++ g10/modul/main.js
@@ -1,40 +1,48 @@
-const user = [
+
+//  const mentor = [
+//     { id: 1, name: "eshmat" },
+//     { id: 1, name: "eshmat" },
+//     { id: 1, name: "eshmat" },
+//     { id: 1, name: "eshmat" },
+//     { id: 1, name: "eshmat" },
+//     { id: 1, name: "eshmat" },
+//     { id: 1, name: "eshmat" },
+//     { id: 1, name: "eshmat" },
+//     { id: 1, name: "eshmat" },
+//     { id: 1, name: "eshmat" },
+//     { id: 1, name: "eshmat" },
+//     { id: 1, name: "eshmat" },
+//     { id: 1, name: "eshmat" },
+//     { id: 1, name: "eshmat" },
+//     { id: 1, name: "eshmat" },
+//     { id: 1, name: "eshmat" },
+//     { id: 1, name: "eshmat" },
+// ];
+// export { user, mentor } 
+// export default user;
+
+let user = [
     { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
+    { id: 2, name: "eshmat" },
+    { id: 3, name: "eshmat" },
+    { id: 4, name: "eshmat" },
+    { id: 5, name: "Toshmat" },
+    { id: 6, name: "eshmat" },
+    { id: 7, name: "eshmat" },
+    { id: 8, name: "eshmat" },
+    { id: 9, name: "eshmat" },
 ];
- const mentor = [
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-    { id: 1, name: "eshmat" },
-];
-export { user, mentor } 
-export default user;
+
+const add = (addData) => {
+    user = [...user, { ...addData }]
+
+}
+add({ name: "Toshmat" })
+console.log(user);
+
+const del = (delData) => {
+    let delUser = user.filter(vl => vl.id !== delData)
+    user = delUser
+}
+del(5)
+console.log(user);
