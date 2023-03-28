0f89da13d2ae6bbb361235db44229dde35f39906
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-03-28 23:05:55 +0500

    DOM - 1 | Kirish

---
+++ g10/DOM/main.js
@@ -0,0 +1,30 @@
+let ul = document.getElementsByTagName("ul")[0]
+// console.log(ul.children);
+// console.log(ul.childNodes);
+
+// console.log(ul.parentNode);
+// console.log(ul.parentElement);
+
+// console.log(ul.firstChild);
+// console.log(ul.lastElementChild);
+
+// console.log(ul.lastElementChild.previousElementSibling.previousElementSibling.nextElementSibling);
+
+// Selector
+
+// let by = document.getElementsByTagName("body")[0]
+// let parent = document.getElementsByClassName("parent")
+// let id = document.getElementById("bir")
+// let qr = document.querySelector("#bir")
+// let qrALL = document.querySelectorAll("#bir")
+// // qr.innerHTML = "<h1>Hey</h1>"
+
+// // console.log(id.style.color = "red");
+// // console.log(qr.innerHTML);
+// // console.log(qr.innerText);
+// // console.log(qr.textContent);
+
+// "bir".hdden = false
+
+
+
