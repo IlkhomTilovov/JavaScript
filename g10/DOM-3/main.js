63e1c125c14765dfe1e5517b6771ec7b5f5a9fa3
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-03-30 18:36:29 +0500

    DOM-2

---
+++ g10/DOM-3/main.js
@@ -0,0 +1,19 @@
+// const inputEls = document.getElementsByName("userName")
+// const inputEl = inputEls[0]
+// console.log(inputEl);
+// inputEl.remove()
+
+// const heading = document.getElementsByClassName("heading")
+// const head1 = heading[0]
+// console.log(head1);
+// head1.remove()
+
+// const headings = document.getElementsByTagName("h1")
+// console.log(headings);
+// headings[1].remove()
+
+// const heading = document.querySelector("#headings")
+// heading.remove()
+
+const headings = document.querySelectorAll("h1")
+console.log(headings);
