5e7709c5aa64c17c8c5fc625e5986ec8800c7067
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-04-05 18:58:19 +0500

    JSga Kirish | Variables | Data Types

---
+++ g12/Data type/main.js
@@ -0,0 +1,39 @@
+// Js nima ?
+// Single Thread nima ?
+// Clean code
+// Variable
+// Variable declortion
+// Data type turlari va nimalar
+
+// let a = 4;
+// let b;
+// let c;
+// c = b = a;
+// console.log(c, b, a);
+
+// let var const
+
+// primitive
+// 1 Number Sonlar
+// 2 String "", '' ,``
+// 3 Bigint 2**53
+// 4 Boolean true va false
+// 5 Null
+// 6 undefined
+// 7 Symbl
+
+// let a;
+// console.log(a);
+// Non primitive
+// Object { }
+// Array [ ]
+// function get() {}
+
+let n1 = null;
+let n2 = null;
+console.log(n1 === n2);
+console.log("=======");
+let a = 2;
+let obj1 = { a };
+let obj2 = { a };
+console.log(obj1 === obj2);
