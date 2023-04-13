f4b430ec0e13f070bdf3d8c218e9150002a4ec70
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-04-13 15:30:26 +0500

    For loop

---
+++ g12/Swicht va For loop/main.js
@@ -0,0 +1,57 @@
+// let a = 5;
+// let b = 3;
+// console.log(a > b);
+// let obhavo = "issiq";
+// switch (obhavo) {
+//   case "yomgir":
+//     console.log("Bugun ob-havo yomg'ir");
+//     break;
+//   case "bulut":
+//     console.log("bugun ob-havo bulutli");
+//     break;
+//   case "issiq":
+//     console.log("Bugun ob-havo issiq");
+//     break;
+//   default:
+//     console.log("No data");
+// }
+
+// for (let a = 1; a <= 7; a += 3) {
+//   console.log(a);
+// }
+
+// let a = 2;
+// for (a; a <= 10; a++) {
+//   console.log(` ${a} karraliklar n/`);
+//   for (let i = 2; i < 10; i++) {
+//     console.log(`${a} * ${i} = ${a * i}`);
+//   }
+// }
+// let k = 10;
+// for (let n = 1; n <= k; n++) {
+//   console.log(`${k}`);
+// }
+
+// let a = 8;
+// let b = 20;
+
+// for (a; a <= b; a++) {
+//   console.log(a);
+// }
+
+// let k = 1;
+
+// let s = 3;
+
+// for (k; k <= 10; k++) {
+//   console.log(`${k} - kg  *  ${s} - narx = ${k * s}sum`);
+// }
+
+// let k = 1;
+
+// let s = 5;
+
+// for (k; k <= 10; k++) {
+//   console.log(`${k / 10} - kg  *  ${s} - narx = ${Math.ceil((k / 10) * s)}sum`);
+// }
+
