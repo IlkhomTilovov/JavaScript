7f1e7526f7576c439992fcf9966b645724f7ef5d
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-04-16 22:22:30 +0500

    Object-1 | Homewors

--- g12/object-1/main.js
+++ g12/object-1/main.js
@@ -36,19 +36,97 @@
 
 // 4-msiol ===============================================
 
-let menu = {
-  width: 200,
-  height: 300,
-  title: "My menu",
-};
+// let menu = {
+//   width: 200,
+//   height: 300,
+//   title: "My menu",
+// };
 
-const kara = (obj) => {
-  let res = "";
-  for (i in obj) {
-    if (typeof obj[i] === "number") {
-      obj[i] = obj[i] * 2;
+// const kara = (obj) => {
+//   let res = "";
+//   for (i in obj) {
+//     if (typeof obj[i] === "number") {
+//       obj[i] = obj[i] * 2;
+//     }
+//   }
+//   console.log(obj);
+// };
+// kara(menu);
+
+// 5-msiol ===============================================
+
+// object keylarini toping
+// let obj = {
+//   id: 1,
+//   name: "WebBrain",
+// };
+// for (i in obj) {
+//   console.log(i);
+// }
+
+// 6-msiol ===============================================
+
+// object valuelarining raqamga tenglarini toping
+// let obj = {
+//   id: 1,
+//   key: 123,
+//   name: "WebBrain",
+// };
+
+// for (i in obj) {
+//   if (typeof obj[i] === "number") {
+//     console.log(`${i} :`, obj[i]);
+//   }
+// }
+
+// 7-msiol ===============================================
+
+// object valuelarining boolean tipidagi malumotlrini qaytaring keylari bn biriga
+// let obj = {
+//   id: 1,
+//   name: "WebBrain",
+//   offline: true,
+//   online: true,
+//   individual: false,
+// };
+
+// for(i in obj){
+//     if(typeof obj[i]=== "boolean"){
+//         console.log(`${i} :`, obj[i]);
+//     }
+// }
+
+// 8-msiol ===============================================
+
+// object va string berilgan .agar object keylari ichida
+// stringga teng bolgan key bolsa shu key va valueni chiqazing
+let obj = {
+  id: 1,
+  name: "WebBrain",
+  offline: true,
+  online: true,
+  individual: false,
+};
+let res = "";
+// function Search(obj, str) {
+//   for (i in obj) {
+//     `${(str = obj[i])}`;
+//     if (str == obj[i]) {
+//       console.log(str);
+//     } else {
+//       console.log("error");
+//     }
+//   }
+// }
+// Search(obj, "1");
+
+function Search(obj, val) {
+  let res = {};
+  for (key in obj) {
+    if (obj[key] === val) {
+      res[key] = val;
     }
   }
-  console.log(obj);
-};
-kara(menu);
+  console.log(res);
+}
+Search(obj, 1);
