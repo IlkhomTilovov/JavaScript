81ed841c9ad5d80661be3c59a5e2b5dac07a725d
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-04-16 15:53:15 +0500

    Functions | Homeworks

---
+++ g12/Function/main.js
@@ -0,0 +1,26 @@
+// const getNum = (a, b) => {
+//   if (a < b) {
+//     console.log(a);
+//   } else {
+//     console.log(b);
+//   }
+// };
+// getNum(2, 5);
+// getNum(3, -1);
+// getNum(1, 1);
+
+// const getPow = (a, b) => {
+//   if (a ** b) {
+//     console.log(a ** b);
+//   }
+// };
+
+// getPow(3, 2);
+// getPow(3, 3);
+// getPow(1, 100);
+
+// const ask = (question, yes, no) => {
+//   if (confirm(question)) yes();
+//   else no();
+// };
+// ask("do you agree?",)
