3d6db7bb8d4dc6128e35e77740998683df01d755
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-04-28 01:34:59 +0500

    Tasks

--- masalalar/main.js
+++ masalalar/main.js
@@ -1,12 +1,58 @@
-function makes10(a, b) {
-  if (a || b == 10) {
-    return true;
-  } else if (a + b === 10) {
-    return true;
-  } else {
-    return false;
+// function makes10(a, b) {
+//   if (a || b == 10) {
+//     return true;
+//   } else if (a + b === 10) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// console.log(makes10(9, 10));
+// console.log(makes10(9, 9));
+// console.log(makes10(8, 3));
+
+// 2)
+// function fun(a, b) {
+//   if (a < b) {
+//     console.log(b);
+//   } else {
+//     console.log(a);
+//   }
+// }
+// fun(50, 6);
+// fun(5, 100);
+
+// 3)
+// const fun = (str) => {
+//   let len = str.length / 2;
+//   let res = str.substring(len, len + 1);
+//   if (str.length % 2 === 0) {
+//     return str.substring(len - 1, len + 1);
+//   } else {
+//     return str.substring(len, len + 1);
+//   }
+// };
+// console.log(fun("1234567890"));
+
+// 4 =======================================
+
+// const fun = (str) => {
+//   let res = "";
+//   for (let i in str) {
+//     if (i % 2 === 0) {
+//       res += str[i];
+//     }
+//   }
+//   console.log(res);
+// };
+// fun("Islom");
+
+// 5 ====================================
+
+const fun = (str) => {
+  let res = "";
+  for (let i in str) {
+    return str[i];
   }
-}
-console.log(makes10(9, 10));
-console.log(makes10(9, 9));
-console.log(makes10(8, 3));
+};
+console.log(fun("Islom"));
