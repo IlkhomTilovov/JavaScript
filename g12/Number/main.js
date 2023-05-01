d09149bb4c49207662348bffc468b7af80f21867
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-05-01 16:20:33 +0500

    Number | HomeWork

---
+++ g12/Number/main.js
@@ -0,0 +1,20 @@
+// const byner = (n) => {
+//   let by = n.toString(2);
+//   let bir = "";
+//   let nol = "";
+//   for (i in by) {
+//     switch (by[i]) {
+//       case "1":
+//         bir += "1";
+//         break;
+//       case "0":
+//         nol += 0;
+//     }
+//   }
+//   console.log(by);
+//   console.log(`${bir.length} ta 1 qatnashgan: ${bir} `);
+//   console.log(`${nol.length} ta 0 qatnashgan: ${nol}`);
+// };
+// byner(7);
+
+let min= 
