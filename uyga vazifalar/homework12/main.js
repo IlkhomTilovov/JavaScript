43f2bbfe8564e854eb2f1546c5df659e34855428
Ilkhom Tilovov <121874722+IlkhomTilovov@users.noreply.github.com>
2023-05-07 00:33:40 +0500

    Advenced array

--- uyga vazifalar/homework12/main.js
+++ uyga vazifalar/homework12/main.js
@@ -227,23 +227,23 @@
 
 
 
-// const text = (str) => {
-//     let res = ""
-//     let count = 1
-//     for (let i = 0; i < str.length; i++) {
-//         if (str[i] === str[i + 1]) {
-//             count++
-//         } else {
-//             res += str[i]
-//             if (count > 1) {
-//                 res += count
-//                 count = 1
-//             }
-//         }
-//     }
-//     console.log(res)
-// }
-// text("webbraaiiin")
+const text = (str) => {
+    let res = ""
+    let count = 1
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === str[i + 1]) {
+            count++
+        } else {
+            res += str[i]
+            if (count > 1) {
+                res += count
+                count = 1
+            }
+        }
+    }
+    console.log(res)
+}
+text("webbraaiiin")
 
 // 10=======================================
 
